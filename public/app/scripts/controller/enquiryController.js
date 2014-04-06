9162111d6f9352a4a7f011001cf2cad9c721106d
Fredrik Salin <salin.fredrik@gmail.com>
2014-04-06 19:05:54 +0200

    Added feature for receiving current enquiries for user

--- public/app/scripts/controller/enquiryController.js
+++ public/app/scripts/controller/enquiryController.js
@@ -1,30 +1,26 @@
-carApp.controller('EnquiryCtrl', function($scope,$http) {
+carApp.controller('EnquiryCtrl', function($scope,$http,UserService) {
 
+		$scope.userID = UserService.id;
+
+	$scope.getCars = function() {
+		//Fetch cars from database
 		$http({method: 'GET', url: 'http://reversecarbay.local/json/cars'}).
 		success(function (data, status, headers, config) {
 			$scope.cars=data;
+			$scope.selectedBrand = $scope.cars[0].brand_name;
 
 			$scope.carBrands = {};
 
-		/*	$scope.cars.forEach(function(entry) { 
-
-				if($scope.carBrands[entry.brand_name] == null) {
-					//Initiate the variable
-					$scope.carBrands[entry.brand_name] = [];
-					$scope.carBrands[entry.brand_name].push("car_id" + ': '+ entry.car_id +"model_name:" + ': '+ entry.model_name);
-				} else {
-					$scope.carBrands[entry.brand_name].push('"car_id"' + ': ' + entry.car_id +'"model_name"' + ': ' + entry.model_name);
-				}
-			}); */
-
+			// Function for restructuring the cars array to be used when you select car model depending on brand
 			$scope.cars.forEach(function(entry) { 
 
-				//Check if brand name has to be 
+				//Check if brand  has to be initialised
 				if($scope.carBrands[entry.brand_name] == null) {
 					
 					$scope.carBrands[entry.brand_name] = {};
 
 				}
+				//Check if model has to be initialised
 				if($scope.carBrands[entry.brand_name][entry.model_name] == null) {
 					$scope.carBrands[entry.brand_name][entry.model_name] = {};
 				}
@@ -37,6 +33,25 @@ carApp.controller('EnquiryCtrl', function($scope,$http) {
 		error(function (data, status, headers, config) {
 
 		});
+
+	};
+
+	$scope.getEnquiries = function() {
+
+		$http({method: 'GET', url: 'http://reversecarbay.local/json/enquiries'}).
+		success(function (data, status, headers, config) {
+			$scope.enquiries=data;
+
+		}).
+		error(function (data, status, headers, config) {
+
+		});
+
+	};
+
+	// Call functions to be run at pageload
+	$scope.getCars();
+	$scope.getEnquiries();
 
 	//Add enquiry to database
 	$scope.addEnquiry=function(formData){
