e167461be401ad21351cb0569c1809a15de04772
Fredrik Salin <salin.fredrik@gmail.com>
2014-04-06 17:35:56 +0200

    Added feature for chosing car model depending on which car brand picked

--- public/app/scripts/controller/enquiryController.js
+++ public/app/scripts/controller/enquiryController.js
@@ -1,5 +1,43 @@
 carApp.controller('EnquiryCtrl', function($scope,$http) {
 
+		$http({method: 'GET', url: 'http://reversecarbay.local/json/cars'}).
+		success(function (data, status, headers, config) {
+			$scope.cars=data;
+
+			$scope.carBrands = {};
+
+		/*	$scope.cars.forEach(function(entry) { 
+
+				if($scope.carBrands[entry.brand_name] == null) {
+					//Initiate the variable
+					$scope.carBrands[entry.brand_name] = [];
+					$scope.carBrands[entry.brand_name].push("car_id" + ': '+ entry.car_id +"model_name:" + ': '+ entry.model_name);
+				} else {
+					$scope.carBrands[entry.brand_name].push('"car_id"' + ': ' + entry.car_id +'"model_name"' + ': ' + entry.model_name);
+				}
+			}); */
+
+			$scope.cars.forEach(function(entry) { 
+
+				//Check if brand name has to be 
+				if($scope.carBrands[entry.brand_name] == null) {
+					
+					$scope.carBrands[entry.brand_name] = {};
+
+				}
+				if($scope.carBrands[entry.brand_name][entry.model_name] == null) {
+					$scope.carBrands[entry.brand_name][entry.model_name] = {};
+				}
+					$scope.carBrands[entry.brand_name][entry.model_name] = entry.car_id;
+		
+			});
+
+
+		}).
+		error(function (data, status, headers, config) {
+
+		});
+
 	//Add enquiry to database
 	$scope.addEnquiry=function(formData){
 		$http({method: 'POST', url: 'http://reversecarbay.local/json/enquiries', data: formData}).
@@ -13,3 +51,4 @@ carApp.controller('EnquiryCtrl', function($scope,$http) {
 	}
 	
 });
+
