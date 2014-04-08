62801221ae60bec032545ae75f5558aa27e17f1d
Fredrik Salin <salin.fredrik@gmail.com>
2014-04-08 10:12:41 +0200

    Add feature for displaying bids connected to enquiries

--- public/app/scripts/controller/enquiryController.js
+++ public/app/scripts/controller/enquiryController.js
@@ -2,8 +2,14 @@ carApp.controller('EnquiryCtrl', function($scope,$http,UserService) {
 
 		$scope.userID = UserService.id;
 
+	// Get the user id from UserService
+	$scope.getUserID = function(){
+			return $scope.userID;
+	}
+
+	//Fetch cars from database
 	$scope.getCars = function() {
-		//Fetch cars from database
+		
 		$http({method: 'GET', url: 'http://reversecarbay.local/json/cars'}).
 		success(function (data, status, headers, config) {
 			$scope.cars=data;
@@ -53,17 +59,42 @@ carApp.controller('EnquiryCtrl', function($scope,$http,UserService) {
 	$scope.getCars();
 	$scope.getEnquiries();
 
+	$scope.getBids = function(enquiryID) {
+
+		if(enquiryID == $scope.selectedEnquiry) {
+			//If user presses button again the field should be hidden
+			$scope.selectedEnquiry = 0;
+		} else {
+		$scope.selectedEnquiry = enquiryID;
+		}
+
+		$http({method: 'GET', url: 'http://reversecarbay.local/json/bids'}).
+			success(function (data, status, headers, config) {
+			$scope.bids=data;
+
+			}).
+			error(function (data, status, headers, config) {
+
+		});
+	}
+
 	//Add enquiry to database
 	$scope.addEnquiry=function(formData){
 		$http({method: 'POST', url: 'http://reversecarbay.local/json/enquiries', data: formData}).
 		success(function (data, status, headers, config) {
 			console.log("Success!");
 
+		// Update enquiries on page
+		$scope.getEnquiries();
+
 		}).
 		error(function (data, status, headers, config) {
 			alert("The enquiry failed");
 		});
+
 	}
+
+			$scope.selectedEnquiry = 0;
 	
 });
 
