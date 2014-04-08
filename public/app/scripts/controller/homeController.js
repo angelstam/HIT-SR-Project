35dfc14d46d8091c9139e5e650f85c100a5ee9a2
Johan Angelstam <johan@angelstam.se>
2014-04-08 12:57:44 +0800

    Add confirm bid dialog.

--- public/app/scripts/controller/homeController.js
+++ public/app/scripts/controller/homeController.js
@@ -3,7 +3,6 @@ carApp.controller('HomeCtrl', function($scope, $http, UserService) {
 	// create a message to display in our view
 	$scope.message = 'Everyone come and see how good I look!';
 
-	$scope.enquiries = [];
 
 	$scope.getAgeFromYear = function($year) {
 		return (new Date()).getFullYear() - parseInt($year);
@@ -22,7 +21,29 @@ carApp.controller('HomeCtrl', function($scope, $http, UserService) {
 	};
 	$scope.loadEnquiries();
 
-	$scope.placeBid = function(form) {
-		console.log(form);
-	}
+	$scope.showPlaceBid = function(enquiry) {
+		$scope.bidEnquiry = enquiry;
+		$scope.placeBid = true;
+	};
+
+	$scope.hidePlaceBid = function() {
+		$scope.bidEnquiry = [];
+		$scope.placeBid = false;
+	};
+
+	$scope.placeBid = function(data) {
+		console.log(data);
+		$http.get('/json/enquiries')
+			.success(function(data) {
+				console.log(data);
+				$scope.enquiries = data;
+		})
+			.error(function(data) {
+			console.log(data);
+		});
+	};
+
+	$scope.enquiries = [];
+	$scope.placeBid = false;
+	$scope.bidEnquiry = [];
 });
