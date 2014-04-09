ba4ab025618592a4259cf8a2ddf50fe935d92316
Johan Angelstam <johan@angelstam.se>
2014-04-09 20:21:55 +0800

    Modal Test that is functional with the bid view. NOT integrated in app however.

--- public/app/app.js
+++ public/app/app.js
@@ -13,7 +13,34 @@ angular.module('mgcrea.ngStrapDocs')
   });
 })
 
-.controller('ModalDemoCtrl', function($scope) {
+.controller('ModalDemoCtrl', function($scope, $http) {
   $scope.modal = {title: 'Place Bid', content: 'Hello Modal<br />This is a multiline message!'};
+  $scope.enquiries = [];
+
+	$scope.getAgeFromYear = function($year) {
+		return (new Date()).getFullYear() - parseInt($year);
+	};
+
+	$scope.loadEnquiries = function() {
+
+		$http.get('/json/enquiries')
+			.success(function(data) {
+				console.log(data);
+				$scope.enquiries = data;
+		})
+			.error(function(data) {
+			console.log(data);
+		});
+	};
+	$scope.loadEnquiries();
+	
+	$scope.showPlaceBid = function(enquiry) {
+		// Show when some event occurs (use $promise property to ensure the template has been loaded)
+		/*$scope.modal.$promise.then(function() {
+			$scope.modal.show();
+		});*/
+		$scope.bidEnquiry = enquiry;
+		//$scope.placeBid = true;
+	};
 });
 
